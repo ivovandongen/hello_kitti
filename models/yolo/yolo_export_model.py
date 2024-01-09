53e6b53114c743f8d39d852460b6ccb16d1fd6ca
Ivo van Dongen <info@ivovandongen.nl>
2024-01-09 11:50:45 +0200

    [detections] add yolo v8 segmentation model

--- models/yolo/yolo_export_model.py
+++ models/yolo/yolo_export_model.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 from ultralytics import YOLO
 
-model = YOLO('yolov8n.pt')
-model.export(format='coreml')
-model.export(format='onnx')
+for file in ['yolov8n.pt', 'yolov8n-seg.pt']:
+    model = YOLO(file)
+    model.export(format='coreml')
+    model.export(format='onnx')
